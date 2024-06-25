d58ea4703b9b7a7decddafdfa3d174128adf1e8a
vasilkovalyov <vasilkovalyov@gmail.com>
2024-06-25 09:57:59 +0200

    add temp component top-search

---
+++ src/blocks/top-search/index.ts
@@ -0,0 +1,1 @@
+export { default as BlockTopSearch } from './top-search';
---
+++ src/blocks/top-search/top-search.tsx
@@ -0,0 +1,11 @@
+export default function TopSearch() {
+  return (
+    <div className="top-search">
+      <div className="top-search__container">
+        <div className="top-search__toggler"></div>
+        <div className="top-search__filters"></div>
+        <div className="top-search__placeholders"></div>
+      </div>
+    </div>
+  );
+}
