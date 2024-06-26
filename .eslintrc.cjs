1a0bcee4c6040fd31a6448dc8b21d685b49c4215
vasilkovalyov <vasilkovalyov@gmail.com>
2024-06-26 13:45:48 +0200

    setup test library

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'react', 'prettier'],
   rules: {
@@ -16,4 +16,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    setupFiles: './src/tests/setup.ts',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
