cfb5e7c9bbd3c9dda97965b19bec641fbfa35dca
vasilkovalyov <vasilkovalyov@gmail.com>
2024-07-02 12:45:53 +0200

    refactoring

--- src/blocks/hotels/hotels.tsx
+++ src/blocks/hotels/hotels.tsx
@@ -1,4 +1,4 @@
-import { FilterPanel, HotelCard, hotelModel } from '@/components';
+import { HotelCard, hotelModel } from '@/components';
 import { BlockSidebarFilter } from '../sidebar-filter';
 import { Button, IconEnum } from '@/components/ui';
 
