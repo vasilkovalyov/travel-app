321fb0813885598f1954119191459d29e4097383
vasilkovalyov <vasilkovalyov@gmail.com>
2024-07-04 16:34:04 +0200

    updated functionality filter-toggler

--- src/components/filter-toggler/filter-toggler.tsx
+++ src/components/filter-toggler/filter-toggler.tsx
@@ -1,69 +1,89 @@
 import cn from 'classnames';
 import { FilterTogglerType } from './filter-toggler.type';
 import { Input, Button } from '../ui';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, forwardRef, ForwardedRef } from 'react';
 
 import './filter-toggler.scss';
 
-export default function FilterToggler({
-  label,
-  text = '',
-  placeholder,
-  onBlur,
-  onFocus,
-}: FilterTogglerType) {
-  const inputRef = useRef<{ field: HTMLInputElement }>(null);
+const FilterToggler = forwardRef(
+  (
+    {
+      label,
+      text = '',
+      placeholder,
+      readonly,
+      onBlur,
+      onFocus,
+    }: FilterTogglerType,
+    ref: ForwardedRef<HTMLInputElement>,
+  ) => {
+    const inputRef = useRef<{ field: HTMLInputElement }>(null);
 
-  const [isFocusedInput, setIsFocusedInput] = useState<boolean>(false);
-  const [inputValue, setInputValue] = useState<string>('');
+    const [isFocusedInput, setIsFocusedInput] = useState<boolean>(false);
+    const [inputValue, setInputValue] = useState<string>('');
 
-  const modificationCn = cn({
-    'filter-toggler--focused': isFocusedInput,
-    'filter-toggler--filled': inputValue.length,
-  });
+    const modificationCn = cn({
+      'filter-toggler--focused': isFocusedInput,
+      'filter-toggler--filled': inputValue.length,
+    });
 
-  const classnames = cn('filter-toggler', modificationCn);
+    const classnames = cn('filter-toggler', modificationCn);
 
-  useEffect(() => {
-    setInputValue(text);
-  }, []);
+    useEffect(() => {
+      setInputValue(text);
+    }, []);
 
-  useEffect(() => {
-    if (isFocusedInput && inputRef.current) {
-      inputRef.current.field.focus();
+    useEffect(() => {
+      setInputValue(text);
+    }, [text]);
+
+    useEffect(() => {
+      if (isFocusedInput && inputRef.current) {
+        inputRef.current.field.focus();
+        onFocus && onFocus();
+      }
+    }, [isFocusedInput]);
+
+    function onCustomBlurInput() {
+      setIsFocusedInput(false);
+      onBlur && onBlur();
+    }
+
+    function onFocusButton() {
+      setIsFocusedInput(true);
       onFocus && onFocus();
     }
-  }, [isFocusedInput]);
 
-  return (
-    <div className={classnames}>
-      <label className="filter-toggler__label text-truncate">{label}</label>
-      {isFocusedInput ? (
-        <Input
-          ref={inputRef as any}
-          autoComplete="off"
-          placeholder={placeholder}
-          value={inputValue}
-          className="filter-toggler__text-field"
-          largeSize
-          clearTextButton
-          onCustomBlur={() => {
-            setIsFocusedInput(false);
-            onBlur && onBlur();
-          }}
-          onChange={(e) => setInputValue(e.currentTarget.value)}
-        />
-      ) : (
-        <Button
-          view="transparent"
-          className="filter-toggler__button"
-          onFocus={() => {
-            setIsFocusedInput(true);
-          }}
-        >
-          {inputValue}
-        </Button>
-      )}
-    </div>
-  );
-}
+    return (
+      <div ref={ref} className={classnames}>
+        <label className="filter-toggler__label text-truncate">{label}</label>
+        {isFocusedInput ? (
+          <Input
+            ref={inputRef as any}
+            autoComplete="off"
+            placeholder={placeholder}
+            value={inputValue}
+            className="filter-toggler__text-field"
+            largeSize
+            clearTextButton
+            readOnly={readonly}
+            onCustomBlur={onCustomBlurInput}
+            onChange={(e) => setInputValue(e.currentTarget.value)}
+          />
+        ) : (
+          <Button
+            view="transparent"
+            className="filter-toggler__button"
+            onFocus={onFocusButton}
+          >
+            {inputValue}
+          </Button>
+        )}
+      </div>
+    );
+  },
+);
+
+FilterToggler.displayName = 'FilterToggler';
+
+export default FilterToggler;
--- src/components/filter-toggler/filter-toggler.type.ts
+++ src/components/filter-toggler/filter-toggler.type.ts
@@ -2,6 +2,8 @@ export type FilterTogglerType = {
   label: string;
   text?: string;
   placeholder?: string;
+  readonly?: boolean;
   onFocus?: () => void;
   onBlur?: () => void;
+  onClick?: () => void;
 };
