f0453e3a5f061ca37aef16b967e888991d0a8606
vasilkovalyov <vasilkovalyov@gmail.com>
2024-07-05 18:15:53 +0200

    refactored modal, expanded functionality

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import cn from 'classnames';
 import { Button, IconEnum } from '../ui';
 import { ModalProps } from './modal.type';
 import { ReactPortal } from '../portal';
@@ -9,6 +10,9 @@ export default function Modal({
   children,
   title,
   open,
+  headerContent,
+  bottomContent,
+  className,
   onHandleClose,
 }: ModalProps) {
   const onHandleCloseEscapeKey = (e: KeyboardEvent) =>
@@ -25,19 +29,27 @@ export default function Modal({
 
   return (
     <ReactPortal wrapperId="react-portal-modal-container">
-      <div className="modal">
+      <div className={cn('modal', className)}>
         <div className="modal__overlay overlay" onClick={onHandleClose}></div>
         <div className="modal__box">
-          <div className="modal__heading">
-            <h3 className="modal__title">{title}</h3>
-            <Button
-              iconSize={22}
-              view="transparent"
-              icon={IconEnum.CROSS}
-              onClick={onHandleClose}
-            />
+          <div className="modal__header">
+            <div className="modal__header-heading">
+              <h3 className="modal__title">{title}</h3>
+              <Button
+                iconSize={22}
+                view="transparent"
+                icon={IconEnum.CROSS}
+                onClick={onHandleClose}
+              />
+            </div>
+            {headerContent && (
+              <div className="modal__header-embed">{headerContent}</div>
+            )}
           </div>
           <div className="modal__content">{children}</div>
+          {bottomContent && (
+            <div className="modal__footer">{bottomContent}</div>
+          )}
         </div>
       </div>
     </ReactPortal>
--- src/components/modal/modal.type.ts
+++ src/components/modal/modal.type.ts
@@ -1,6 +1,9 @@
 export type ModalProps = {
-  title?: string;
+  title?: string | null;
   open: boolean;
   children?: React.ReactNode;
+  headerContent?: React.ReactNode;
+  bottomContent?: React.ReactNode;
+  className?: string;
   onHandleClose: () => void;
 };
